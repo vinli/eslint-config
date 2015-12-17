521f80f331538ba69959273b71c66736fd77cc6c
Kyle Turney <kyle@dialexa.com>
2015-12-17 13:18:49 -0600

    changed wrong require rule

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ module.exports = merge({}, require('./index'), {
 
     // Node.js and CommonJS
     'callback-return': 2,
-    'global-require': 2,
+    'global-require': 0,
     'handle-callback-err': 2,
-    'no-mixed-requires': 0,
+    'no-mixed-requires': 2,
     'no-new-require': 2,
     'no-path-concat': 2,
     'no-process-exit': 2,
